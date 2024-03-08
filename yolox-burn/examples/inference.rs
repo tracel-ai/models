9a8851c6f10c81faf05e4c4ee629d535a289c2e1
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-03-08 11:02:14 -0500

    Add post-processing and inference results

--- yolox-burn/examples/inference.rs
+++ yolox-burn/examples/inference.rs
@@ -1,7 +1,10 @@
-use yolox_burn::model::{weights, yolox::Yolox};
+use std::path::Path;
+
+use image::{DynamicImage, ImageBuffer};
+use yolox_burn::model::{boxes::nms, weights, yolox::Yolox, BoundingBox};
 
 use burn::{
-    backend::NdArray,
+    backend::LibTorch,
     tensor::{backend::Backend, Data, Device, Element, Shape, Tensor},
 };
 
@@ -19,13 +22,82 @@ fn to_tensor<B: Backend, T: Element>(
         .swap_dims(1, 0) // [C, H, W]
 }
 
+/// Draws bounding boxes on the given image.
+///
+/// # Arguments
+///
+/// * `image`: Original input image.
+/// * `boxes` - Bounding boxes, grouped per class.
+/// * `color` - [R, G, B] color values to draw the boxes.
+/// * `ratio` - [x, y] aspect ratio to scale the predicted boxes.
+///
+/// # Returns
+///
+/// The image annotated with bounding boxes.
+fn draw_boxes(
+    image: DynamicImage,
+    boxes: &[Vec<BoundingBox>],
+    color: &[u8; 3],
+    ratio: &[f32; 2], // (x, y) ratio
+) -> DynamicImage {
+    // Assumes x1 <= x2 and y1 <= y2
+    fn draw_rect(
+        image: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+        x1: u32,
+        x2: u32,
+        y1: u32,
+        y2: u32,
+        color: &[u8; 3],
+    ) {
+        for x in x1..=x2 {
+            let pixel = image.get_pixel_mut(x, y1);
+            *pixel = image::Rgb(*color);
+            let pixel = image.get_pixel_mut(x, y2);
+            *pixel = image::Rgb(*color);
+        }
+        for y in y1..=y2 {
+            let pixel = image.get_pixel_mut(x1, y);
+            *pixel = image::Rgb(*color);
+            let pixel = image.get_pixel_mut(x2, y);
+            *pixel = image::Rgb(*color);
+        }
+    }
+
+    // Annotate the original image and print boxes information.
+    let (image_h, image_w) = (image.height(), image.width());
+    let mut image = image.to_rgb8();
+    for (class_index, bboxes_for_class) in boxes.iter().enumerate() {
+        for b in bboxes_for_class.iter() {
+            let xmin = (b.xmin * ratio[0]).clamp(0., image_w as f32 - 1.);
+            let ymin = (b.ymin * ratio[1]).clamp(0., image_h as f32 - 1.);
+            let xmax = (b.xmax * ratio[0]).clamp(0., image_w as f32 - 1.);
+            let ymax = (b.ymax * ratio[1]).clamp(0., image_h as f32 - 1.);
+
+            println!(
+                "Predicted {} ({:.2}) at [{:.2}, {:.2}, {:.2}, {:.2}]",
+                class_index, b.confidence, xmin, ymin, xmax, ymax,
+            );
+
+            draw_rect(
+                &mut image,
+                xmin as u32,
+                xmax as u32,
+                ymin as u32,
+                ymax as u32,
+                color,
+            );
+        }
+    }
+    DynamicImage::ImageRgb8(image)
+}
+
 pub fn main() {
     // Parse arguments
     let img_path = std::env::args().nth(1).expect("No image path provided");
 
     // Create YOLOX-Nano
     let device = Default::default();
-    let model: Yolox<NdArray> = Yolox::yolox_s_pretrained(weights::YoloxS::Coco, &device)
+    let model: Yolox<LibTorch> = Yolox::yolox_s_pretrained(weights::YoloxS::Coco, &device)
         .map_err(|err| format!("Failed to load pre-trained weights.\nError: {err}"))
         .unwrap();
 
@@ -52,6 +124,23 @@ pub fn main() {
     // Forward pass
     let out = model.forward(x);
 
-    // TODO: post-processing and display
-    println!("Output shape: {:?}", out.shape());
+    // Post-processing
+    let [_, num_boxes, num_outputs] = out.shape().dims;
+    let boxes = out.clone().slice([0..1, 0..num_boxes, 0..4]);
+    let obj_scores = out.clone().slice([0..1, 0..num_boxes, 4..5]);
+    let cls_scores = out.slice([0..1, 0..num_boxes, 5..num_outputs]);
+    let scores = cls_scores * obj_scores;
+    let boxes = nms(boxes, scores, 0.65, 0.5);
+
+    // Draw outputs and save results
+    let (h, w) = (img.height(), img.width());
+    let img_out = draw_boxes(
+        img,
+        &boxes[0],
+        &[239u8, 62u8, 5u8],
+        &[w as f32 / WIDTH as f32, h as f32 / HEIGHT as f32],
+    );
+
+    let img_path = Path::new(&img_path);
+    let _ = img_out.save(img_path.with_extension("output.png"));
 }
---
+++ yolox-burn/src/model/boxes.rs
@@ -0,0 +1,139 @@
+use burn::tensor::{backend::Backend, ElementConversion, Tensor};
+use itertools::Itertools;
+
+pub struct BoundingBox {
+    pub xmin: f32,
+    pub ymin: f32,
+    pub xmax: f32,
+    pub ymax: f32,
+    pub confidence: f32,
+}
+
+/// Non-maximum suppression (NMS) filters overlaping bounding boxes that have an intersection-over-
+/// union (IoU) greater or equal than the specified `iou_threshold` with previously selected boxes.
+///
+/// Boxes are filtered based on `score_threshold` and ranked based on their score. As such, lower
+/// scoring boxes are removed when overlapping with another (higher scoring) box.
+///
+/// # Arguments
+///
+/// * `boxes`: Bounding box coordinates. Shape: `[batch_size, num_boxes, 4]`.
+/// * `scores` - Classification scores for each box. Shape: `[batch_size, num_boxes, num_classes]`.
+/// * `iou_threshold` - Scalar threshold for IoU.
+/// * `score_threshold` - Scalar threshold for scores.
+///
+/// # Returns
+///
+/// Vector of bounding boxes grouped by class for each batch. The boxes are sorted in decreasing
+/// order of scores for each class.
+pub fn nms<B: Backend>(
+    boxes: Tensor<B, 3>,
+    scores: Tensor<B, 3>,
+    iou_threshold: f32,
+    score_threshold: f32,
+) -> Vec<Vec<Vec<BoundingBox>>> {
+    let [batch_size, num_boxes, num_classes] = scores.shape().dims;
+
+    // Bounding boxes grouped by batch and by (maximum) class index
+    let mut bboxes = boxes
+        .iter_dim(0)
+        .zip(scores.iter_dim(0))
+        .enumerate()
+        // Per-batch
+        .map(|(_, (candidate_boxes, candidate_scores))| {
+            // Keep max scoring boxes only ([num_boxes, 1], [num_boxes, 1])
+            let (cls_score, cls_idx) = candidate_scores.squeeze::<2>(0).max_dim_with_indices(1);
+            let cls_score: Vec<_> = cls_score
+                .into_data()
+                .value
+                .iter()
+                .map(|v| v.elem::<f32>())
+                .collect();
+            let cls_idx: Vec<_> = cls_idx
+                .into_data()
+                .value
+                .iter()
+                .map(|v| v.elem::<i64>() as usize)
+                .collect();
+
+            // [num_boxes, 4]
+            let candidate_boxes: Vec<_> = candidate_boxes
+                .into_data()
+                .value
+                .iter()
+                .map(|v| v.elem::<f32>())
+                .collect();
+
+            // Per-class filtering based on score
+            (0..num_classes)
+                .map(|cls_id| {
+                    // [num_boxes, 1]
+                    (0..num_boxes)
+                        .filter_map(|box_idx| {
+                            let box_cls_idx = cls_idx[box_idx];
+                            if box_cls_idx != cls_id {
+                                return None;
+                            }
+                            let box_cls_score = cls_score[box_idx];
+                            if box_cls_score >= score_threshold {
+                                let bbox = &candidate_boxes[box_idx * 4..box_idx * 4 + 4];
+                                // println!("Candidate {}: {:?}", box_idx, bbox);
+                                Some(BoundingBox {
+                                    xmin: bbox[0] - bbox[2] / 2.,
+                                    ymin: bbox[1] - bbox[3] / 2.,
+                                    xmax: bbox[0] + bbox[2] / 2.,
+                                    ymax: bbox[1] + bbox[3] / 2.,
+                                    confidence: box_cls_score,
+                                })
+                            } else {
+                                None
+                            }
+                        })
+                        .sorted_unstable_by(|a, b| a.confidence.partial_cmp(&b.confidence).unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    for batch_idx in 0..batch_size {
+        non_maximum_suppression(&mut bboxes[batch_idx], iou_threshold);
+    }
+
+    bboxes
+}
+
+/// Intersection over union of two bounding boxes.
+pub fn iou(b1: &BoundingBox, b2: &BoundingBox) -> f32 {
+    let b1_area = (b1.xmax - b1.xmin + 1.) * (b1.ymax - b1.ymin + 1.);
+    let b2_area = (b2.xmax - b2.xmin + 1.) * (b2.ymax - b2.ymin + 1.);
+    let i_xmin = b1.xmin.max(b2.xmin);
+    let i_xmax = b1.xmax.min(b2.xmax);
+    let i_ymin = b1.ymin.max(b2.ymin);
+    let i_ymax = b1.ymax.min(b2.ymax);
+    let i_area = (i_xmax - i_xmin + 1.).max(0.) * (i_ymax - i_ymin + 1.).max(0.);
+    i_area / (b1_area + b2_area - i_area)
+}
+
+/// Perform non-maximum suppression over boxes of the same class.
+pub fn non_maximum_suppression(bboxes: &mut [Vec<BoundingBox>], threshold: f32) {
+    for bboxes_for_class in bboxes.iter_mut() {
+        bboxes_for_class.sort_by(|b1, b2| b2.confidence.partial_cmp(&b1.confidence).unwrap());
+        let mut current_index = 0;
+        for index in 0..bboxes_for_class.len() {
+            let mut drop = false;
+            for prev_index in 0..current_index {
+                let iou = iou(&bboxes_for_class[prev_index], &bboxes_for_class[index]);
+                if iou > threshold {
+                    drop = true;
+                    break;
+                }
+            }
+            if !drop {
+                bboxes_for_class.swap(current_index, index);
+                current_index += 1;
+            }
+        }
+        bboxes_for_class.truncate(current_index);
+    }
+}
