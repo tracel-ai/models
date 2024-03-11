5637aaf6081cd76c23f874ae2f298be77585ae5b
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-03-11 08:05:27 -0400

    Use the new tensor.permute()

--- yolox-burn/examples/inference.rs
+++ yolox-burn/examples/inference.rs
@@ -17,9 +17,8 @@ fn to_tensor<B: Backend, T: Element>(
     device: &Device<B>,
 ) -> Tensor<B, 3> {
     Tensor::<B, 3>::from_data(Data::new(data, Shape::new(shape)).convert(), device)
-        // permute(2, 0, 1)
-        .swap_dims(2, 1) // [H, C, W]
-        .swap_dims(1, 0) // [C, H, W]
+        // [H, W, C] -> [C, H, W]
+        .permute([2, 0, 1])
 }
 
 /// Draws bounding boxes on the given image.
