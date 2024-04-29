5a49503c82d0ef17f6502de69f6c62b4a06d7154
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-04-29 09:13:20 -0400

    Add finetuning example

--- resnet-burn/README.md
+++ resnet-burn/README.md
@@ -28,6 +28,8 @@ resnet-burn = { git = "https://github.com/tracel-ai/models", package = "resnet-b
 
 ### Example Usage
 
+#### Inference
+
 The [inference example](examples/inference/examples/inference.rs) initializes a ResNet-18 from the
 ImageNet
 [pre-trained weights](https://pytorch.org/vision/stable/models/generated/torchvision.models.resnet18.html#torchvision.models.ResNet18_Weights)
@@ -37,4 +39,36 @@ You can run the example with the following command:
 
 ```sh
 cargo run --release --example inference samples/dog.jpg --release
+```
+
+#### Fine-tuning
+
+For this [multi-label image classification fine-tuning example](examples/finetune), a sample of the
+planets dataset from the Kaggle competition
+[Planet: Understanding the Amazon from Space](https://www.kaggle.com/c/planet-understanding-the-amazon-from-space)
+is downloaded from a
+[fastai mirror](https://github.com/fastai/fastai/blob/master/fastai/data/external.py#L55). The
+sample dataset is a collection of satellite images with multiple labels describing the scene, as
+illustrated below.
+
+<img src="./samples/dataset.jpg" alt="Planet dataset sample" width="1000"/>
+
+To achieve this task, a ResNet-18 pre-trained on the ImageNet dataset is fine-tuned on the target
+planets dataset. The training recipe used is fairly simple. The main objective is to demonstrate how to re-use a
+pre-trained model for a different downstream task.
+
+Without any bells and whistle, our model achieves over 90% multi-label accuracy (i.e., hamming
+score) on the validation set after just 5 epochs and over 95% after 10 epochs.
+
+Run the example with the Torch GPU backend:
+
+```sh
+export TORCH_CUDA_VERSION=cu121
+cargo run --release --example finetune --features tch-gpu
+```
+
+Run it with our WGPU backend:
+
+```sh
+cargo run --release --example finetune --features wgpu
 ```
---
+++ resnet-burn/examples/finetune/src/data.rs
@@ -0,0 +1,132 @@
+use burn::{
+    data::{
+        dataloader::batcher::Batcher,
+        dataset::vision::{Annotation, ImageDatasetItem, PixelDepth},
+    },
+    prelude::*,
+};
+
+use super::dataset::CLASSES;
+
+// ImageNet mean and std values
+const MEAN: [f32; 3] = [0.485, 0.456, 0.406];
+const STD: [f32; 3] = [0.229, 0.224, 0.225];
+
+// Planets patch size
+const WIDTH: usize = 256;
+const HEIGHT: usize = 256;
+
+/// Create a multi-hot encoded tensor.
+///
+/// # Example
+///
+/// ```rust, ignore
+/// let multi_hot = multi_hot::<B>(&[2, 5, 8], 10, &device);
+/// println!("{}", multi_hot.to_data());
+/// // [0, 0, 1, 0, 0, 1, 0, 0, 1, 0]
+/// ```
+pub fn multi_hot<B: Backend>(
+    indices: &[usize],
+    num_classes: usize,
+    device: &B::Device,
+) -> Tensor<B, 1, Int> {
+    Tensor::zeros(Shape::new([num_classes]), device).scatter(
+        0,
+        Tensor::from_ints(
+            indices
+                .iter()
+                .map(|i| *i as i32)
+                .collect::<Vec<_>>()
+                .as_slice(),
+            device,
+        ),
+        Tensor::ones(Shape::new([indices.len()]), device),
+    )
+}
+
+/// Normalizer with ImageNet values as it helps accelerate training since we are fine-tuning from
+/// ImageNet pre-trained weights and the model expects the data to be in this normalized range.
+#[derive(Clone)]
+pub struct Normalizer<B: Backend> {
+    pub mean: Tensor<B, 4>,
+    pub std: Tensor<B, 4>,
+}
+
+impl<B: Backend> Normalizer<B> {
+    /// Creates a new normalizer.
+    pub fn new(device: &Device<B>) -> Self {
+        let mean = Tensor::from_floats(MEAN, device).reshape([1, 3, 1, 1]);
+        let std = Tensor::from_floats(STD, device).reshape([1, 3, 1, 1]);
+        Self { mean, std }
+    }
+
+    /// Normalizes the input image according to the ImageNet dataset.
+    ///
+    /// The input image should be in the range [0, 1].
+    /// The output image will be in the range [-1, 1].
+    ///
+    /// The normalization is done according to the following formula:
+    /// `input = (input - mean) / std`
+    pub fn normalize(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
+        (input - self.mean.clone()) / self.std.clone()
+    }
+}
+
+#[derive(Clone)]
+pub struct ClassificationBatcher<B: Backend> {
+    normalizer: Normalizer<B>,
+    device: B::Device,
+}
+
+#[derive(Clone, Debug)]
+pub struct ClassificationBatch<B: Backend> {
+    pub images: Tensor<B, 4>,
+    pub targets: Tensor<B, 2, Int>,
+}
+
+impl<B: Backend> ClassificationBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self {
+            normalizer: Normalizer::<B>::new(&device),
+            device,
+        }
+    }
+}
+
+impl<B: Backend> Batcher<ImageDatasetItem, ClassificationBatch<B>> for ClassificationBatcher<B> {
+    fn batch(&self, items: Vec<ImageDatasetItem>) -> ClassificationBatch<B> {
+        fn image_as_vec_u8(item: ImageDatasetItem) -> Vec<u8> {
+            // Convert Vec<PixelDepth> to Vec<u8> (Planet images are u8)
+            item.image
+                .into_iter()
+                .map(|p: PixelDepth| -> u8 { p.try_into().unwrap() })
+                .collect::<Vec<u8>>()
+        }
+
+        let targets = items
+            .iter()
+            .map(|item| {
+                // Expect multi-hot encoded class labels as target (e.g., [0, 1, 0, 0, 1])
+                if let Annotation::MultiLabel(y) = &item.annotation {
+                    multi_hot(y, CLASSES.len(), &self.device)
+                } else {
+                    panic!("Invalid target type")
+                }
+            })
+            .collect();
+
+        let images = items
+            .into_iter()
+            .map(|item| Data::new(image_as_vec_u8(item), Shape::new([HEIGHT, WIDTH, 3])))
+            .map(|data| Tensor::<B, 3>::from_data(data.convert(), &self.device).permute([2, 0, 1]))
+            .map(|tensor| tensor / 255) // normalize between [0, 1]
+            .collect();
+
+        let images = Tensor::stack(images, 0);
+        let targets = Tensor::stack(targets, 0);
+
+        let images = self.normalizer.normalize(images);
+
+        ClassificationBatch { images, targets }
+    }
+}
